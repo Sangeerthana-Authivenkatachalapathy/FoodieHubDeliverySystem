7474fbf741ff4a9b65b08dbfe28a5662c11e61de
Kavitha-Sankar <2417829@cognizant.com>
2025-07-07 14:52:20 +0530

    commit changes

--- FoodieHubDeliverySystem.Repository/Enums/PaymentStatus.cs
+++ FoodieHubDeliverySystem.Repository/Enums/PaymentStatus.cs
@@ -12,37 +12,9 @@ using System.Threading.Tasks;
 
 namespace FoodieHubDeliverySystem.Repository.Enums
 {
-<<<<<<< HEAD
-<<<<<<<< HEAD:FoodieHubDeliverySystem.Repository/Services/RestaurantService.cs
-    public class RestaurantService : IRestaurantService
-    {
-        private readonly List<Restaurant> _restaurants;
-        public Restaurant Create (Restaurant restaurant)
-        {
-            restaurant.Id = _restaurants.Count+1;
-            _restaurants.Add(restaurant);
-            return restaurant;
-        }
-        public IEnumerable<Restaurant> GetAll ()
-        {
-            return _restaurants;
-        }
-        public Restaurant GetById (int id)
-        {
-            return _restaurants.FirstOrDefault(r => r.Id == id);
-        }
-        public void Delete (int id)
-        {
-            var restaurant = GetById(id);
-            if (restaurant != null)
-            {
-                _restaurants.Remove(restaurant);
-            }
-        }
-
+<<<<<<<< HEAD:FoodieHubDeliverySystem.Repository/Enums/UserRole.cs
+    internal class UserRole
 ========
-=======
->>>>>>> 87cda9004bb817b7e4b1671514eb4b02eefd9943
     public enum PaymentStatus
     {
         Pending,
