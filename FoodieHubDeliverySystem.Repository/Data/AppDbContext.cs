884ba21f913ea01ebfde2affb640312cdc3e3130
Sangeerthana-Authivenkatachalapathy <sangeerthana.authivenkatachalapathy@cognizant.com>
2025-07-07 12:57:23 +0530

    Update AppDbContext.cs

--- FoodieHubDeliverySystem.Repository/Data/AppDbContext.cs
+++ FoodieHubDeliverySystem.Repository/Data/AppDbContext.cs
@@ -6,7 +6,6 @@ namespace FoodieHubDeliverySystem.Data
     public class AppDbContext: DbContext
     {
         public object Restaurant;
-
         public DbSet<Restaurant> RestaurantDetails { get; set; }
         public DbSet<RestaurantReview> restaurantReviews { get; set; }
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
